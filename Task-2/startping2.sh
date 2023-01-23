444994a32912a51e3ffd5cf64c4638d1b94656f0
cakossowski <59179770+cakossowski@users.noreply.github.com>
2023-01-23 14:36:03 +0100

    Update startping2.sh
    - removed static IPs
    - added variables to catch IPs of each container, to keep it as dynamic as possible
    - checked if naming is consistent

--- Task-2/startping2.sh
+++ Task-2/startping2.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 # create networks needed for task-2
-docker network create --driver=bridge --subnet=1.1.1.0/16 net1
-docker network create --driver=bridge --subnet=1.1.1.50/16 net2
+docker network create net1
+docker network create net2
 
 # create containers Client1, Client2 and Router1
-docker run -itd --privileged --net net1 --name Client1 --ip 1.1.1.1 ubuntu-ping bash
-docker run -itd --privileged --net net1 --name Router1 --ip 1.1.1.5 ubuntu-ping bash
-docker run -itd --privileged --net net2 --name Client2 --ip 1.1.1.2 ubuntu-ping bash
+docker run -itd --privileged --net net1 --name Client1 ubuntu-ping bash
+docker run -itd --privileged --net net1 --name Router1 ubuntu-ping bash
+docker run -itd --privileged --net net2 --name Client2 ubuntu-ping bash
 
 # add router Router1 to remaining network
-docker network connect net2 R1
+docker network connect net2 Router1
+
+# get IP-Addresses of Router1 in networks as variables for further use
+r1_net1=$(docker container inspect Router1 --format "{{ .NetworkSettings.Networks.net1.IPAddress }}")
+r1_net2=$(docker container inspect Router1 --format "{{ .NetworkSettings.Networks.net2.IPAddress }}")
+
+# get IP-Addresses of each container in their respective network
+c1_net1=$(docker container inspect Client1 --format "{{ .NetworkSettings.Networks.net1.IPAddress }}")
+c2_net2=$(docker container inspect Client2 --format "{{ .NetworkSettings.Networks.net2.IPAddress }}")
+
 
 # change settings for each container to change default gateway
-docker exec -it Client1 ip route replace default via 1.1.1.5
-docker exec -it Client2 ip route replace default via 1.1.1.5
+docker exec -it Client1 ip route replace default via $r1_net1
+docker exec -it Client2 ip route replace default via $r1_net2
 
 # show flow with traceroute and ping for Client 1
 echo "------- Client 1 to Client 2 -------"
-docker exec -it Client1 traceroute 1.1.1.2
-docker exec -it Client1 ping -c 2 1.1.1.2
+docker exec -it Client1 traceroute $c1_net1
+docker exec -it Client1 ping -c 2 $c1_net1
 
 # show flow with traceroute and ping for Client 2
 echo "------- Client 2 to Client 1 -------"
-docker exec -it Client2 traceroute 1.1.1.1
-docker exec -it Client2 ping -c 2 1.1.1.1
+docker exec -it Client2 traceroute $c2_net2
+docker exec -it Client2 ping -c 2 $c2_net2
