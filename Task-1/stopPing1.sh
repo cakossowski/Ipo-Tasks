4a21a2a758bb2e57467f9ba558369f56e4541cd0
cakossowski <59179770+cakossowski@users.noreply.github.com>
2023-01-23 15:19:37 +0100

    Update stopPing1.sh
    - fixed issue where network could not be deleted while active connections were found, by simply killing containers first and then removing network

--- Task-1/stopPing1.sh
+++ Task-1/stopPing1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# shutdown and remove created networks net1/net2
-docker network rm net1
-
 # kill and remove created containers
 docker kill Client1 Client2 && docker rm Client1 Client2
+
+# shutdown and remove created networks net1/net2
+docker network rm net1
 
 echo "Network removed. Containers killed and removed"
