ff5efcd1ce42afffadd79fa1b144703b5622908f
cakossowski <59179770+cakossowski@users.noreply.github.com>
2023-01-23 14:46:17 +0100

    Update startping1.sh
    - script was restructured to match naming conventions 
    - variables added
    - checked consistent naming
    - added traceroute / ping part

--- Task-1/startping1.sh
+++ Task-1/startping1.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# create networks needed for task-2
-docker network create --driver=bridge --subnet=1.1.1.0/16 netz1
-docker network create --driver=bridge --subnet=1.1.1.50/16 netz2
+# create network needed for task-1
+docker network create net1
 
-# create containers C1, C2 (Clients) and R1 (Router)
-docker run -itd --privileged --net netz1 --name Client1 --ip 1.1.1.1 ubuntu-ping bash
-docker run -itd --privileged --net netz1 --name Router1 --ip 1.1.1.5 ubuntu-ping bash
-docker run -itd --privileged --net netz2 --name Client2 --ip 1.1.1.2 ubuntu-ping bash
+# create containers Client1, Client2
+docker run -itd --privileged --net net1 --name Client1 ubuntu-ping bash
+docker run -itd --privileged --net net1 --name Client2 ubuntu-ping bash
 
-# add router R1 to remaining network
-docker network connect netz2 R1
+# get IP-Addresses of each container in their respective network
+c1_net1=$(docker container inspect Client1 --format "{{ .NetworkSettings.Networks.net1.IPAddress }}")
+c2_net1=$(docker container inspect Client2 --format "{{ .NetworkSettings.Networks.net1.IPAddress }}")
 
 
 # change settings for each container to change default gateway
+docker exec -it Client1 ip route replace default via $c2_net1
+docker exec -it Client2 ip route replace default via $c1_net1
+
+# show flow with traceroute and ping for Client 1
+echo "------- Client 1 to Client 2 -------"
+docker exec -it Client1 traceroute $c2_net1
+docker exec -it Client1 ping -c 2 $c2_net1
+
+# show flow with traceroute and ping for Client 2
+echo "------- Client 2 to Client 1 -------"
+docker exec -it Client2 traceroute $c1_net1
+docker exec -it Client2 ping -c 2 $c1_net1
